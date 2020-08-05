79b159425cd3882f11bbd441594922b04d630e4b
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 21:13:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,16 @@ require "pry"
 class Dog 
 
 def name= (dog_name)
-  this_dog_name= dog_name 
+  @this_dog_name= dog_name 
 end 
 
 def name 
-  this_dog_name
+  @this_dog_name
 end 
-
+binding.pry 
 end 
 
 
 lessie= Dog.new 
 lessie.name= "Lassie" 
-lessie.name 
+puts lessie.name 
